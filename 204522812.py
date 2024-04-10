c94dc5c2c4a8056b3067406ac23c5653d15b6171
NetAcad DEVASC <changeme@example.com>
2024-04-10 23:31:41 +0000

    arch python

---
+++ 204522812.py
@@ -0,0 +1,13 @@
+VLAN = int(input("numero de vlan:" ))
+nativevlan1 = [10,20,30,99]
+nativevlan2 = [40,50,60,200]
+VLAN
+
+if nativevlan1 == VLAN:
+    print("pertenece al switch1")
+else:
+    print("no pertenece al swtich1")
+if nativevlan2 == VLAN:
+    print("pertenece al switch2")
+else:
+    print("no pertenece al swtich2")
---
+++ ithan_aguilar.py
@@ -0,0 +1,8 @@
+Nombre = input("nombre:")
+Apellido = input("apellido:")
+rut = input("rut:")
+Duoc_email = input("Duoc email:")
+Duoc_sede = input("Duoc sede:")
+print (Nombre +" "+ Apellido +" "+ rut +" "+ Duoc_email +" "+ Duoc_sede)
+
+
